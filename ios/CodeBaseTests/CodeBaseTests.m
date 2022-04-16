846dd7c5e3182c08806c0c2c6e2d9f0c4ffb1e0c
thaiducanh <thaianh19020208@gmail.com>
2022-04-16 09:30:30 +0700

    config chatapp

--- ios/CodeBaseTests/CodeBaseTests.m
+++ ios/CodeBaseTests/CodeBaseTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface CodeBaseTests : XCTestCase
+@interface ChatAppTests : XCTestCase
 
 @end
 
-@implementation CodeBaseTests
+@implementation ChatAppTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
